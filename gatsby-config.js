e1d8c96495852fc7de382eee3a0941c27397292d
Jason-Ng00 <e0407783@u.nus.edu>
2021-07-13 16:24:47 +0800

    removed gatsby-cloud plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,8 +51,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
-    `gatsby-plugin-gatsby-cloud`,
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
