36c75427d145bf0c91fffa3f6e94b9dfe18382f7
Jason-Ng00 <e0407783@u.nus.edu>
2021-07-30 17:23:34 +0800

    fix warnings

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,13 @@ import React from 'react'
 import * as styles from './Header.module.scss'
 import { Container } from 'react-bootstrap'
 import { StaticImage } from "gatsby-plugin-image"
-import { Link } from "gatsby"
 
 
 export default function Header() {
   return (
     <Container className={styles.header}>
 
-      <Link to="http://www.lib.nus.edu.sg/">
+      <a href="http://www.lib.nus.edu.sg/">
       <StaticImage
         src={"../../images/NUSL_logo.png"}
         alt="NUS LOGO"
@@ -18,7 +17,7 @@ export default function Header() {
         objectFit="contain"
       />
 
-      </Link>
+      </a>
 
       </Container>
 
